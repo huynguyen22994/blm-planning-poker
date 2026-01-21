54354e1f570c0ec15fc0561493547e23ee9315d4
Huy Nguyen <huy.nguyen22994@gmail.com>
2026-01-21 20:18:42 +0700

    feat: Update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,4 +18,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: '../backend/public',
+    assetsDir: 'static',
+    emptyOutDir: true,
+  }
 }));
